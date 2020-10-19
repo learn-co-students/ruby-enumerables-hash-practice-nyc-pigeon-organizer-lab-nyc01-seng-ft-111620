7baff5814dc6a25e2657bb127d7b307eaa62ff6a
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-19 18:07:26 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  #result = {}
+  
+  data.each_with_object({}) do |(key, values), result|
+    values.each do |attributes, names|
+      names.each do |name|
+        if result[name] == nil
+          result[name] = {}
+        end
+        if !result[name][key]
+          !result[name][key] = []
+        end
+        result[name][key].push(attributes.to_s)
+      end
+    end
+  #result
+  #binding.pry
+  end
 end
+ 
+
+
