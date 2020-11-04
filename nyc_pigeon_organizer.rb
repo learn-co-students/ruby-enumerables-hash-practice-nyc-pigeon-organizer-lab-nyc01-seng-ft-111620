af5d3effca67f63b273ec4e10ce9a5e15464ba1d
Okechukwu O Uneze <unezeo@gmail.com>
2020-11-04 00:27:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+  data.map do |k1,kv|
+    kv.each do |k, v|
+      v.map do |x|
+        hash[x] ||= {}
+        hash[x][k1] ||= []
+        hash[x][k1] << k.to_s
+      end
+    end
+  end
+  return hash
 end
