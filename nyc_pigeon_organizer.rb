792d880afdedb2da7fffbfbc6163f04447f3b07f
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-05 21:10:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,25 +1,57 @@
 require 'pry'
 
-def nyc_pigeon_organizer(data)
-  
-  #result = {}
-  
-  data.each_with_object({}) do |(key, values), result|
-    values.each do |attributes, names|
-      names.each do |name|
+# def nyc_pigeon_organizer(data)
+#   data.each_with_object({}) do |(key, values), result|
+#     values.each do |attributes, names|
+#       names.each do |name|
+#         if result[name] == nil
+#           result[name] = {}
+#         end
+#         if !result[name][key]
+#           !result[name][key] = []
+#         end
+#         result[name][key].push(attributes.to_s)
+#       end
+#     end
+#   end
+# end
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def nyc_pigeon_organizer(pigeon_hash)
+  result = {}
+  pigeon_hash.each do |color_gender_lives, attributes|
+    attributes.each do |characteristics, array_names|
+      array_names.each do |name|
         if result[name] == nil
           result[name] = {}
         end
-        if !result[name][key]
-          !result[name][key] = []
+        if result[name][color_gender_lives] == nil
+          result[name][color_gender_lives] = []
         end
-        result[name][key].push(attributes.to_s)
+        result[name][color_gender_lives].push(characteristics.to_s)
       end
     end
-  #result
-  #binding.pry
-  end
+  end 
+  result
 end
- 
+
+
+
+
 
 
