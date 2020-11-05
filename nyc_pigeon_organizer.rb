b47ebff836586f30695798c99111d1d8f89f7e10
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-05 19:36:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, value), final_hash|
+    value.each do |inner_key, names|
+     names.each do |name|
+       if !final_hash[name]
+         final_hash[name] = {}
+        end
+        if !final_hash[name][key]
+          final_hash[name][key] = []
+        end
+        final_hash[name][key].push (inner_key.to_s)
+    end
+    end
+  end
 end
