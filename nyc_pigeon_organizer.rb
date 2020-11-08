22a826622afab50285272454d9b6c01e6a611e43
Erwin Feng <efeng732@gmail.com>
2020-11-08 16:49:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+new_hash = {}
+  data.each do |color_gender_lives, values|
+    values.each do |value, array|
+      array.each do |name|
+  if new_hash[name] == nil
+      new_hash[name] = {}
+  end 
+  if new_hash[name][color_gender_lives] == nil
+     new_hash[name][color_gender_lives] = [] 
+  end 
+  new_hash[name][color_gender_lives].push(value.to_s)
+    
+    
+        
+end 
+end
+end
+new_hash
 end
